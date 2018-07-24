63cea1fd96a7a3606a0a9e22823eb20259b00f74
Daksha Patel <dakshapatel100@gmail.com>
2018-07-24 17:16:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
 
-    if params[:username].empty? && || params[:password].empty?
+    if params[:username].empty? || params[:password].empty?
       redirect to '/failure'
     end
 
